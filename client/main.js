ad3cbd751e58333ad98541d7c7a8aedcd7fb5509
Nelson Mamani <mamani.charle@gmail.com>
2015-06-21 17:53:57 -0400

    Refactored product form to use AutoForm package

--- client/main.js
+++ client/main.js
@@ -19,3 +19,13 @@ Router.route('/orders', function () {
 Router.route('/help', function () {
     this.render('help');
 });
+
+//Schema validation Messages
+
+SimpleSchema.messages({
+  required: "[label] es requerido",
+  minString: "[label] debe contener por lo menos [min] caracteres",
+  maxString: "[label] no puede exceder los  [max] caracteres",
+  minNumber: "[label] debe ser como minimo [min]",
+  maxNumber: "[label] no pude ser mas que [max]"
+});
--- client/templ/products.js
+++ client/templ/products.js
@@ -1,6 +1,14 @@
+Session.setDefault('productToEdit', null);
+
 Template.products.helpers({
     productList: function () {
         return Products.find();
+    }
+});
+
+Template.productForm.helpers({
+    newEditProduct: function () {
+        return Products.findOne({_id: Session.get('productToEdit')});
     }
 });
 
@@ -10,12 +18,44 @@ Template.productForm.events({
         var prodType = tmpl.find('#typeInput').value;
         var prodDesc = tmpl.find('#descInput').value;
         var prodPrice = tmpl.find('#priceInput').value;
-        var prodState = tmpl.find('#stateInput').value;
-        if (prodName) {
-            Products.insert({id: Counters.getNextSecuence('productId'),name:prodName, description: prodDesc, type: prodType, price: prodPrice, state: prodState});
+        if (AutoForm.validateForm('insertProductForm')) {
+            var recordId = Session.get('productToEdit');
+            if (recordId) {//editing
+                Products.update(recordId,
+                {
+                    $set:{
+                        name: prodName,
+                        description: prodDesc,
+                        type: prodType,
+                        price: prodPrice
+                    }
+                });
+            } else {//newRecord
+                var productId = Counters.getNextSecuence('productId');
+                Products.insert({
+                    productId: productId,
+                    name:prodName,
+                    description: prodDesc,
+                    type: prodType,
+                    price: prodPrice
+                });
+            }
+
             tmpl.find('.cancelBtn').click();
-        } else {
-            
         }
+    },
+    'click .cancelBtn' : function() {
+        Session.set('productToEdit', null);
+    },
+    'click .showProductFormBtn' : function () {
+        AutoForm.resetForm('insertProductForm');
+    }
+});
+
+Template.productRow.events({
+    'dblclick ': function (event, tmpl) {
+        console.log('data: ', tmpl);
+        Session.set('productToEdit', tmpl.data._id);
+        $('.showProductFormBtn').click();
     }
 });
